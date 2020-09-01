57e79f51a55595974a094bcaf6087ccb7cda63d1
阿男 <l.weinan@gmail.com>
2020-09-01 17:14:28 +0800

    save-progres

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 fn main() {
+    play_with_point();
     foo();
+}
+
+fn play_with_point() {
+    let p1 = Point {x:5, y:10.4};
+    let p2 = Point {x:"Hello", y: 'c'};
+    let p3 = p1.mixup(p2);
+    println!("{:?}", p3);
+}
+
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn no_dangle() -> String {
